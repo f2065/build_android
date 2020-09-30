383f8888257e4707f08fa06218a88a1d5f5b6e82
f2065 <f2065@mail.ru>
2020-09-30 17:15:52 +0300

    fix path for sudo

--- clean_all.sh
+++ clean_all.sh
@@ -6,43 +6,43 @@ fi
 
 printf "\033]2;%s Clean temp15...\007" $(date +"%Y-%m-%d_%H:%M:%S")
 echo "Clean temp15..."
-e4defrag ~/temp15
+e4defrag /home/f2065/temp15
 vmware-toolbox-cmd disk wipe /home/f2065/temp15
 
 printf "\033]2;%s Clean TWRP...\007" $(date +"%Y-%m-%d_%H:%M:%S")
 echo "Clean TWRP..."
-rm -rf ~/src_M2_TWRP_LOS/out/
-e4defrag ~/src_M2_TWRP_LOS
+rm -rf /home/f2065/src_M2_TWRP_LOS/out/
+e4defrag /home/f2065/src_M2_TWRP_LOS
 vmware-toolbox-cmd disk wipe /home/f2065/src_M2_TWRP_LOS
 
 printf "\033]2;%s Clean LineageOS 14.1...\007" $(date +"%Y-%m-%d_%H:%M:%S")
 echo "Clean LineageOS 14.1..."
-rm -rf ~/src_M2_LOS141/out/
-e4defrag ~/src_M2_LOS141
+rm -rf /home/f2065/src_M2_LOS141/out/
+e4defrag /home/f2065/src_M2_LOS141
 vmware-toolbox-cmd disk wipe /home/f2065/src_M2_LOS141
 
 printf "\033]2;%s Clean LineageOS 15.1...\007" $(date +"%Y-%m-%d_%H:%M:%S")
 echo "Clean LineageOS 15.1..."
-rm -rf ~/src_M2_LOS151/out/
-e4defrag ~/src_M2_LOS151
+rm -rf /home/f2065/src_M2_LOS151/out/
+e4defrag /home/f2065/src_M2_LOS151
 vmware-toolbox-cmd disk wipe /home/f2065/src_M2_LOS151
 
 printf "\033]2;%s Clean LineageOS 16.0...\007" $(date +"%Y-%m-%d_%H:%M:%S")
 echo "Clean LineageOS 16.0..."
-rm -rf ~/src_M2_LOS160/out/
-e4defrag ~/src_M2_LOS160
+rm -rf /home/f2065/src_M2_LOS160/out/
+e4defrag /home/f2065/src_M2_LOS160
 vmware-toolbox-cmd disk wipe /home/f2065/src_M2_LOS160
 
 printf "\033]2;%s Clean LineageOS 17.1...\007" $(date +"%Y-%m-%d_%H:%M:%S")
 echo "Clean LineageOS 17.1..."
-rm -rf ~/src_M2_LOS171/out/
-e4defrag ~/src_M2_LOS171
+rm -rf /home/f2065/src_M2_LOS171/out/
+e4defrag /home/f2065/src_M2_LOS171
 vmware-toolbox-cmd disk wipe /home/f2065/src_M2_LOS171
 
 #printf "\033]2;%s Clean AICP 13.1...\007" $(date +"%Y-%m-%d_%H:%M:%S")
 #echo "Clean AICP 13.1..."
-#rm -rf ~/src_M2_AICP131/out/
-#e4defrag ~/src_M2_AICP131
+#rm -rf /home/f2065/src_M2_AICP131/out/
+#e4defrag /home/f2065/src_M2_AICP131
 #vmware-toolbox-cmd disk wipe /home/f2065/src_M2_AICP131
 
 exit
